504a3ea0522f7b73c741f7ec1ec211499e51962a
moeom <moeom@192.168.1.13>
2026-09-18 15:48:35 +0900

    fix: Okamoto Case02.javaにコメントを追加
    可読性上げるためコメント追記

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -62,25 +62,31 @@ public class Case02 {
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
+		//DB未登録のログインID入力、IDが正しく入力されていることを確認
 		WebElement idElement = webDriver.findElement(By.id("loginId"));
 		idElement.clear();
 		idElement.sendKeys("StudentABC01");
 		assertEquals("StudentABC01", idElement.getAttribute("value"), "IDで指定した要素の値が正しく入力されていること");
 
+		//DB未登録のパスワード入力、パスワードが正しく入力されていることを確認
 		WebElement passIdElement = webDriver.findElement(By.id("password"));
 		passIdElement.clear();
 		passIdElement.sendKeys("StudentABC01");
 		assertEquals("StudentABC01", passIdElement.getAttribute("value"), "IDで指定した要素の値が正しく入力されていること");
 
+		//ログインボタンを取得しクリック
 		WebElement btnCssElement = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
 		btnCssElement.click();
 
+		//待ち処理
 		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(60));
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".help-inline.error")));
 
+		//ログイン失敗時のエラーメッセージを取得
 		WebElement errorClassElement = webDriver.findElement(By.cssSelector(".help-inline.error"));
 		assertEquals("* ログインに失敗しました。", errorClassElement.getText(), "エラーメッセージが正しいこと");
 
+		//エビデンス取得
 		WebDriverUtils.getEvidence(new Object() {
 		}, "case02_02");
 
