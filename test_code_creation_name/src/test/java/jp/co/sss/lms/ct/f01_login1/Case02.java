b63942a6bcfc28b526b1db770522d94b66bf0f55
moeom <moeom@192.168.1.13>
2026-09-18 15:07:55 +0900

    fix: Okamoto Case.02 受講生ログイン認証失敗のテストコード追加、結合テスト仕様書にCase.02までを追記
    Test.01：トップページにアクセスしログイン画面が表示されることを確認する
    Test.02：DBに登録されていないユーザーでログインし、エラーメッセージが表示されることを確認する

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,11 +12,17 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能①
  * ケース02
- * @author holy
+ * @author 岡本もえ
  */
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース02 受講生 ログイン 認証失敗")
@@ -31,18 +40,50 @@ public class Case02 {
 		closeDriver();
 	}
 
+	/** Test01 */
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// ログイン画面を開く
+		webDriver.get("http://localhost:8080/lms/");
+
+		assertEquals("http://localhost:8080/lms/", webDriver.getCurrentUrl());
+
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(60));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("loginId")));
+
+		WebDriverUtils.getEvidence(new Object() {
+		}, "case02_01");
 	}
 
+	/** Test02 */
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		WebElement idElement = webDriver.findElement(By.id("loginId"));
+		idElement.clear();
+		idElement.sendKeys("StudentABC01");
+		assertEquals("StudentABC01", idElement.getAttribute("value"), "IDで指定した要素の値が正しく入力されていること");
+
+		WebElement passIdElement = webDriver.findElement(By.id("password"));
+		passIdElement.clear();
+		passIdElement.sendKeys("StudentABC01");
+		assertEquals("StudentABC01", passIdElement.getAttribute("value"), "IDで指定した要素の値が正しく入力されていること");
+
+		WebElement btnCssElement = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		btnCssElement.click();
+
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(60));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".help-inline.error")));
+
+		WebElement errorClassElement = webDriver.findElement(By.cssSelector(".help-inline.error"));
+		assertEquals("* ログインに失敗しました。", errorClassElement.getText(), "エラーメッセージが正しいこと");
+
+		WebDriverUtils.getEvidence(new Object() {
+		}, "case02_02");
+
 	}
 
 }
