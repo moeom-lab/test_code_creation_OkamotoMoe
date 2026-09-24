c35176ebb4c304f7f540f29c2d63a989ca46fc50
moeom <moeom@192.168.1.13>
2026-09-24 11:14:51 +0900

    fix:Okamoto Case03,04 テストコード追加、合わせて結合仕様書追記
    Case03:初回ログイン済みユーザでログインできるか
    Case04:よくある質問画面へ遷移できるか

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,12 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能①
@@ -31,18 +40,53 @@ public class Case03 {
 		closeDriver();
 	}
 
+	/** Test01 */
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// ログイン画面表示確認
+		webDriver.get("http://localhost:8080/lms/");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		//待ち処理
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(60));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("loginId")));
+
+		//エビデンス取得
+		WebDriverUtils.getEvidence(new Object() {
+		}, "case1");
 	}
 
+	/** Test02 */
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//DB登録済みのログインID入力、IDが正しく入力されていることを確認
+		WebElement idElement = webDriver.findElement(By.id("loginId"));
+		idElement.clear();
+		idElement.sendKeys("StudentAB01");
+		assertEquals("StudentAB01", idElement.getAttribute("value"), "IDで指定した要素の値が正しく入力されていること");
+
+		//DB登録済みのパスワード入力、パスワードが正しく入力されていることを確認
+		WebElement passIdElement = webDriver.findElement(By.id("password"));
+		passIdElement.clear();
+		passIdElement.sendKeys("StudentAB011");
+		assertEquals("StudentAB011", passIdElement.getAttribute("value"), "IDで指定した要素の値が正しく入力されていること");
+
+		//ログインボタンを取得しクリック
+		WebElement btnCssElement = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		btnCssElement.click();
+
+		// コース詳細画面表示確認
+		webDriver.get("http://localhost:8080/lms/course/detail");
+
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		//エビデンス取得
+		WebDriverUtils.getEvidence(new Object() {
+		}, "case03_02");
 	}
 
 }
