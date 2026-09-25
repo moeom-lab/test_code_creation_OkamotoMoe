807a922adfa43d7ea4882e53f1ac7a1882bab4e8
moeom <moeom@192.168.1.13>
2026-09-25 11:12:25 +0900

    fix:Okamoto Case.07 レポート新規登録テストコード追加、合わせて仕様書追記
    正常系でレポート提出し、提出ボタン名が変更されるか確認

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,12 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト レポート機能
@@ -35,35 +44,106 @@ public class Case07 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// ログイン画面表示確認
+		webDriver.get("http://localhost:8080/lms/");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		//待ち処理
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(60));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("loginId")));
+
+		//エビデンス取得
+		WebDriverUtils.getEvidence(new Object() {
+		}, "case07_01");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//DB登録済みのログインID入力、IDが正しく入力されていることを確認
+		WebElement idElement = webDriver.findElement(By.id("loginId"));
+		idElement.clear();
+		idElement.sendKeys("StudentAB01");
+		assertEquals("StudentAB01", idElement.getAttribute("value"), "IDで指定した要素の値が正しく入力されていること");
+
+		//DB登録済みのパスワード入力、パスワードが正しく入力されていることを確認
+		WebElement passIdElement = webDriver.findElement(By.id("password"));
+		passIdElement.clear();
+		passIdElement.sendKeys("StudentAB011");
+		assertEquals("StudentAB011", passIdElement.getAttribute("value"), "IDで指定した要素の値が正しく入力されていること");
+
+		//ログインボタンを取得しクリック
+		WebElement btnCssElement = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		btnCssElement.click();
+
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		//エビデンス取得
+		WebDriverUtils.getEvidence(new Object() {
+		}, "case07_02");
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 未提出の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//詳細ボタン押下
+		WebElement detailBtnElement = webDriver.findElement(By.cssSelector("input[value='詳細']"));
+		detailBtnElement.click();
+
+		//待ち処理
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(60));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("sectionDetail")));
+
+		assertEquals("セクション詳細 | LMS", webDriver.getTitle());
+
+		WebDriverUtils.getEvidence(new Object() {
+		}, "case07_03");
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「提出する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+		//提出するボタン押下
+		WebElement submitElement = webDriver.findElement(By.cssSelector("input[value='日報【デモ】を提出する']"));
+		submitElement.click();
+
+		//待ち処理
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(60));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("content_0")));
+
+		assertEquals("レポート登録 | LMS", webDriver.getTitle());
+
+		WebDriverUtils.getEvidence(new Object() {
+		}, "case07_04");
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を入力して「提出する」ボタンを押下し確認ボタン名が更新される")
 	void test05() {
-		// TODO ここに追加
+		//テキストエリア取得・入力
+		WebElement textAreaElement = webDriver.findElement(By.id("content_0"));
+		textAreaElement.clear();
+		textAreaElement.sendKeys("本日の日報");
+
+		//提出するボタン押下
+		WebElement completeSubmitElement = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		completeSubmitElement.click();
+
+		//待ち処理
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(60));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("sectionDetail")));
+
+		//ボタン名変更確認
+		WebElement comfirmElement = webDriver.findElement(By.cssSelector("input[value*='提出済み']"));
+		assertEquals("提出済み日報【デモ】を確認する", comfirmElement.getAttribute("value"));
+
+		WebDriverUtils.getEvidence(new Object() {
+		}, "case07_05");
 	}
 
 }
